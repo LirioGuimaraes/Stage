9fb0801b60354c0de09f5bd5664b69b909acea24
Lirio Guimaraes <lirio12345678@gmail.com>
2024-04-08 15:28:58 +0200

    Lecture des données

---
+++ R.R
@@ -0,0 +1,12 @@
+D1 <- X2023_09_29_ParacouP1LastCensus
+D2 <- X2023_09_29_ParacouP6LastCensus
+D3 <- X2023_09_29_ParacouP11LastCensus
+
+summary(D1)
+
+A <- D1( )
+
+species <- X2023_09_29_ParacouP14AllYears 
+
+sort (table (paste (doc$x,doc$y)))
+
